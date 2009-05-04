d639a7bac5c436a6ce5891877740cf2994c942f0
Jonathan Hoyt <jonmagic@Sarah.local>
2009-05-04 19:26:41 -0400

    fixing qb client import

--- db/utilities/suite_qb_bootstrap/generate_bootstrap.rb
+++ db/utilities/suite_qb_bootstrap/generate_bootstrap.rb
@@ -1,14 +1,7 @@
-require 'rubygems'
-require 'quickbooks'
-
 puts "Job Starting!"
-$bootstrap = File.open('../../../import_clients_from_qb.rb', 'w')
+$bootstrap = File.open('import_clients_from_qb.rb', 'w')
 $bootstrap << File.read('db/utilities/suite_qb_bootstrap/bootstrap_bootstrap.rb')
 $bootstrap << "puts \"\nBootstrap Begin!\n\""
-
-# uncomment the following line (and comment the one below it) and replace with similar line from config/environments/production.rb to connect to QB on a remote computer
-# Quickbooks.use_adapter :https, 'address', 'SuiteCS', 'secret'
-Quickbooks.use_adapter :ole, "SuiteCS"
 
 class Addresser
   def self.parse(qb_address_block)
--- db/utilities/suite_qb_bootstrap/run.bat
+++ db/utilities/suite_qb_bootstrap/run.bat
@@ -1,6 +1,6 @@
 path %PATH%;C:\Program Files\SuiteCS\bin\
 cd "C:\Program Files\SuiteCS\runtime"
-ruby db/utilities/suite_qb_bootstrap/generate_bootstrap.rb
+ruby script/runner db/utilities/suite_qb_bootstrap/generate_bootstrap.rb RAILS_ENV=production
 ruby script/runner import_clients_from_qb.rb RAILS_ENV=production
 del import_clients_from_qb.rb
 pause
