0ecdf0cf8fdbe87047798844b78373bd5952dd9a
Jonathan Hoyt <jonmagic@Sarah.local>
2009-05-04 21:17:44 -0400

    small fix in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :login_required
-  layout 'users', :except => [:show]
+  layout 'users', :except => [:show, :new]
 
   def show
     @user = User.find(current_user.id)
