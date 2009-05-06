fd367670e81dd6377ade6e52b79d84b13503dde3
Jonathan Hoyt <jonmagic@Sarah.local>
2009-05-06 11:41:43 -0400

    updated quickbooks importer

--- db/utilities/suite_qb_bootstrap/bootstrap_bootstrap.rb
+++ db/utilities/suite_qb_bootstrap/bootstrap_bootstrap.rb
@@ -1,15 +1,15 @@
 class Client < ActiveRecord::Base
-  def self.import_qb(qb_id, name, firstname, lastname, company, note, email_address, phone, fax, alt_phone, bill_address, ship_address)
+  def self.import_qb(qb_id, name, firstname, lastname, company, note, email_address, phone, fax, alt_phone, bill_address_street, bill_address_city, bill_address_state, bill_address_zip, ship_address_street, ship_address_city, ship_address_state, ship_address_zip)
     unless Client.find_by_qb_id(qb_id)
-      if client = Client.create(:name => name, :firstname => firstname, :lastname => lastname, :company => company, :note => note)
+      if client = Client.create(:name => name, :firstname => firstname, :lastname => lastname, :company => company, :note => note, :qb_id => qb_id)
         Email.create(:client_id => client.id, :context => 'Primary', :address => email_address) if !email_address.blank?
 
         Phone.create(:client_id => client.id, :context => 'Primary', :number => phone) if !phone.blank?
         Phone.create(:client_id => client.id, :context => 'Fax', :number => fax) if !fax.blank?
         Phone.create(:client_id => client.id, :context => 'Secondary', :number => phone) if !alt_phone.blank?
 
-        Address.create(:client_id => client.id, :context => 'Billing', :full_address => bill_address) if !bill_address.blank?
-        Address.create(:client_id => client.id, :context => 'Shipping', :full_address => bill_address) if !ship_address.blank?
+        Address.create(:client_id => client.id, :context => 'Billing', :thoroughfare => bill_address_street, :city => bill_address_city, :state => bill_address_state, :zip => bill_address_zip) if !bill_address_street.blank?
+        Address.create(:client_id => client.id, :context => 'Shipping', :thoroughfare => ship_address_street, :city => ship_address_city, :state => ship_address_state, :zip => ship_address_zip) if !ship_address_street.blank?
       end
     end
   end
--- db/utilities/suite_qb_bootstrap/generate_bootstrap.rb
+++ db/utilities/suite_qb_bootstrap/generate_bootstrap.rb
@@ -18,9 +18,15 @@ end
 
 # def self.import_qb(qb_id, name, firstname, lastname, company, note, email_address, phone, fax, alt_phone, bill_address, ship_address)
 QB::Customer.each do |customer|
-  bill_address = Addresser.parse(customer[:BillAddressBlock])
-  ship_address = Addresser.parse(customer[:ShipAddressBlock])
-  $bootstrap << "Client.import_qb(#{customer[:ListID].to_s.inspect}, #{customer[:CompanyName].to_s.inspect}, #{customer[:FirstName].to_s.inspect}, #{customer[:LastName].to_s.inspect}, #{!customer[:CompanyName].blank?}, #{customer[:Notes].to_s.inspect}, #{customer[:Email].to_s.inspect}, #{customer[:Phone].to_s.inspect}, #{customer[:Fax].to_s.inspect}, #{customer[:AltPhone].to_s.inspect}, #{bill_address.inspect}, #{ship_address.inspect}) rescue Client.rescue!(#{customer[:FullName].to_s.inspect})\n"
+  bill_address_street   = customer[:BillAddress][:Addr2]
+  bill_address_city     = customer[:BillAddress][:City]
+  bill_address_state    = customer[:BillAddress][:State]
+  bill_address_zip      = customer[:BillAddress][:PostalCode]
+  ship_address_street   = customer[:ShipAddress][:Addr2]
+  ship_address_city     = customer[:ShipAddress][:City]
+  ship_address_state    = customer[:ShipAddress][:State]
+  ship_address_zip      = customer[:ShipAddress][:PostalCode]
+  $bootstrap << "Client.import_qb(#{customer[:ListID].to_s.inspect}, #{customer[:CompanyName].to_s.inspect}, #{customer[:FirstName].to_s.inspect}, #{customer[:LastName].to_s.inspect}, #{!customer[:CompanyName].blank?}, #{customer[:Notes].to_s.inspect}, #{customer[:Email].to_s.inspect}, #{customer[:Phone].to_s.inspect}, #{customer[:Fax].to_s.inspect}, #{customer[:AltPhone].to_s.inspect}, #{bill_address_street.to_s.inspect}, #{bill_address_city.to_s.inspect}, #{bill_address_state.to_s.inspect}, #{bill_address_zip.to_s.inspect}, #{ship_address_street.to_s.inspect}, #{ship_address_city.to_s.inspect}, #{ship_address_state.to_s.inspect}, #{ship_address_zip.to_s.inspect}) rescue Client.rescue!(#{customer[:FullName].to_s.inspect})\n"
 end
 
 Quickbooks.connection.close
