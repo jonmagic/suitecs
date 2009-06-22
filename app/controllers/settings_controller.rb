2dda22fafb319811f7332d21eb5bb1d9084c3f72
Jonathan Hoyt <jonmagic@Nikita.local>
2009-06-22 12:54:43 -0400

    adding settings to suite

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,7 +3,37 @@ class SettingsController < ApplicationController
   layout 'settings'
 
   def index
-    
+    @settings = Setting.find(:all)
+  end
+  
+  def new
+    @setting = Setting.new
+  end
+  
+  def create
+    @setting = Setting.new(params[:setting])
+    if @setting.save
+      flash[:notice] = "Setting created successfully!"
+      redirect_to settings_url
+    else
+      flash[:notice] = @setting.errors.to_s
+      redirect_to new_setting_url
+    end
+  end
+  
+  def edit
+    @setting = Setting.find(params[:id])
+  end
+  
+  def update
+    @setting = Setting.find(params[:id])
+    if @setting.update_attributes(params[:setting])
+      flash[:notice] = "Setting updated successfully!"
+      redirect_to settings_url
+    else
+      flash[:notice] = @setting.errors.to_s
+      redirect_to edit_setting_url(@setting)
+    end
   end
 
 end
