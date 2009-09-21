b78461b25e1c0a6e21a3c6bb5e937271887400f7
Jonathan Hoyt <jonmagic@sarah.(none)>
2009-09-20 21:20:10 -0400

    updated sma to not email between 10pm and 7am

--- app/models/sentry.rb
+++ app/models/sentry.rb
@@ -9,7 +9,11 @@ class Sentry < ActiveRecord::Base
   end
   
   def notify!(subject, message=nil)
-    NotificationMailer.deliver_notification(subject, message, self.schedule)
+    start_time = Time.today + 7.hours
+    end_time = Time.today + 22.hours
+    if Time.now > start_time && Time.now < end_time
+      NotificationMailer.deliver_notification(subject, message, self.schedule)
+    end
   end
   
   def to_json(options={})
