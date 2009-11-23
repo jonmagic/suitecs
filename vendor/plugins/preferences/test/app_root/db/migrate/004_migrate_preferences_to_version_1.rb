5442aa1000ce989a90c636eb7d5ee11af261b0b4
Jonathan Hoyt <jonmagic@sarah.(none)>
2009-11-23 14:12:46 -0500

    removed plugins that aren't needed, and turned off css caching

--- vendor/plugins/preferences/test/app_root/db/migrate/004_migrate_preferences_to_version_1.rb
+++
@@ -1,9 +0,0 @@
-class MigratePreferencesToVersion1 < ActiveRecord::Migration
-  def self.up
-    Rails::Plugin.find(:preferences).migrate(1)
-  end
-  
-  def self.down
-    Rails::Plugin.find(:preferences).migrate(0)
-  end
-end
