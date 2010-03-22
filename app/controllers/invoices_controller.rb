6d53ede4d1c23853b9d4d0286c4ab90a330c3093
Jonathan Hoyt <jonmagic@Sarah.local>
2010-03-22 12:51:30 -0400

    made it so invoiced tickets are automatically re-assigned to customer service and scheduled 7 days in the future.

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -8,7 +8,7 @@ class InvoicesController < ApplicationController
     invoice_saved = false
     unless ticket.client.qb_id.blank?
       invoice = params[:invoice_number].blank? ? Invoice.create_for(ticket) : Invoice.create_for(ticket, params[:invoice_number])
-      invoice_saved = invoice.existing_record?
+      invoice_saved = !invoice.new_record?
     end
 
     if invoice_saved
@@ -16,6 +16,7 @@ class InvoicesController < ApplicationController
       ticket.invoiced = true
       ticket.user_id = User.find_by_name("Customer Service").id
       ticket.active_on = 7.days.from_now.to_date
+      ticket.completed_on = nil
       ticket.save
       invoice[:IsToBePrinted].to_s == "false" ? NotificationMailer.deliver_attention(ticket, invoice) : "No need"
       TicketEntry.create(:entry_type => "Invoice", :note => "Invoice ##{invoice[:RefNumber].to_s}", :billable => false, :private => false, :detail => 1, :ticket => ticket, :creator_id => current_user.id)
