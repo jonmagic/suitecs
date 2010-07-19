0c8fc352bf41fc6a308af3732dff436378da218e
Jonathan Hoyt <jonmagic@gmail.com>
2010-07-19 11:06:22 -0400

    fixed migration to use true instead of 1

--- db/migrate/20100719150015_add_active_boolean_to_user.rb
+++ db/migrate/20100719150015_add_active_boolean_to_user.rb
@@ -1,6 +1,6 @@
 class AddActiveBooleanToUser < ActiveRecord::Migration
   def self.up
-    add_column :users, :active, :boolean, :default => 1
+    add_column :users, :active, :boolean, :default => true
   end
 
   def self.down
