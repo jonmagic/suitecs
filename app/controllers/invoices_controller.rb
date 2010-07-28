c3e0c4341b18798b8e66319f7c20d14390b666aa
Jonathan Hoyt <jonmagic@gmail.com>
2010-07-28 11:00:44 -0400

    turned off customer service ticket return code

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -14,9 +14,9 @@ class InvoicesController < ApplicationController
     if invoice_saved
       Quickbooks.connection.close
       ticket.invoiced = true
-      ticket.user_id = User.find_by_name("Customer Service").id
-      ticket.active_on = 7.days.from_now.to_date
-      ticket.completed_on = nil
+      # ticket.user_id = User.find_by_name("Customer Service").id
+      # ticket.active_on = 7.days.from_now.to_date
+      # ticket.completed_on = nil
       ticket.save
       invoice[:IsToBePrinted].to_s == "false" ? Navvy::Job.enqueue(NotificationMailer, :deliver_attention, ticket.id, invoice[:RefNumber]) : "No need"
       TicketEntry.create(:entry_type => "Invoice", :note => "Invoice ##{invoice[:RefNumber].to_s}", :billable => false, :private => false, :detail => 1, :ticket => ticket, :creator_id => current_user.id)
