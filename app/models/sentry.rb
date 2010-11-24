a16e94bcb510f7ab175a82c051dbf4b849078fbd
Jonathan Hoyt <jonmagic@gmail.com>
2010-11-24 12:20:22 -0500

    made sentry notifications use navvy again

--- app/models/sentry.rb
+++ app/models/sentry.rb
@@ -12,7 +12,7 @@ class Sentry < ActiveRecord::Base
     start_time = Time.today + 7.hours
     end_time = Time.today + 22.hours
     if Time.now > start_time && Time.now < end_time
-      NotificationMailer.deliver_notification(subject, message, self.schedule.id)
+      Navvy::Job.enqueue(NotificationMailer, :deliver_notification, subject, message, self.schedule.id)
     end
   end
 
