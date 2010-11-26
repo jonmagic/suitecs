28f37ae97c996ee0099fd931cacaf354a3e828e6
Jonathan Hoyt <jonmagic@gmail.com>
2010-11-26 12:58:07 -0500

    made client connector pull more results from quickbooks

--- app/controllers/qb_client_connector_controller.rb
+++ app/controllers/qb_client_connector_controller.rb
@@ -1,6 +1,6 @@
 class QbClientConnectorController < ApplicationController
   layout "settings"
-  
+
   def index
     @ajax = false
 
@@ -21,12 +21,12 @@ class QbClientConnectorController < ApplicationController
       @clients = Client.find(:all, :conditions => {:qb_id => nil})
     end
   end
-  
+
   def show
     @client = Client.find(params[:id])
     @client.company? ? name = @client.name : name = @client.lastname
     begin
-      @possibles = QB::Customer.all(:MaxReturned => 5, :NameFilter => {:MatchCriterion => "Contains", :Name => "#{name}"})
+      @possibles = QB::Customer.all(:MaxReturned => 30, :NameFilter => {:MatchCriterion => "Contains", :Name => "#{name}"})
     rescue
       flash[:notice] = "Could not connect to Quickbooks."
       @possibles = []
@@ -34,10 +34,10 @@ class QbClientConnectorController < ApplicationController
       Quickbooks.connection.close
     end
   end
-  
+
   def update
     @client = Client.find(params[:id])
-    
+
     @client.qb_id = params[:client][:qb_id]
     if @client.save
       flash[:notice] = "Client qb_id has been updated!"
